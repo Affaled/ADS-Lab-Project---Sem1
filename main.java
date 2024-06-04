4661b937fe3cd3c1b00e0108a7ad5607d77faa8d
Lucas Affaled <affaledl@gmail.com>
2024-06-04 16:53:18 -0300

    classe endereço completa

--- main.java
+++ main.java
@@ -101,4 +101,80 @@ class Produto {
     public String toString() {
         return String.format("Nome: %s\nPreço: %.2f", this.getNome(), this.getPreco());
     };
+};
+
+class Endereco {
+
+    //Atributos
+    private String nomeDaRua;
+    private String cidade;
+    private String estado;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    //Construtor
+    public Endereco(String nomeDaRua, String cidade, String estado, String cep, String numero, String complemento) {
+        this.nomeDaRua = nomeDaRua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    //Getters
+    public String getNomeDaRua() {
+        return this.nomeDaRua;
+    };
+
+    public String getCidade() {
+        return this.cidade;
+    };
+
+    public String getEstado() { 
+        return this.estado;
+    }; 
+
+    public String getCep() {
+        return this.cep;
+    };
+
+    public String getNumero() {
+        return this.numero;
+    };
+
+    public String getComplemento() {
+        return this.complemento;
+    };
+
+    //Setters
+    public void setNomeDaRua(String nomeDaRua) {
+        this.nomeDaRua = nomeDaRua;
+    };
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    };
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    };
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    };
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    };
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    };
+
+    //toString
+    public String toString() {
+        return String.format("Nome da Rua: %s\nCidade: %s\nEstado: %s\nCEP: %s\nNúmero: %s\nComplemento: %s", this.getNomeDaRua(), this.getCidade(), this.getEstado(), this.getCep(), this.getNumero(), this.getComplemento());
+    }
 }
